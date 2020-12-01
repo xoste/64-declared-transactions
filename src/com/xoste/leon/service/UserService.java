c8a790a93eb2c6574628a0bb99b565a89b0a70c2
xoste <1594884214@qq.com>
2020-12-01 16:40:07 +0800

    Initial commit

---
+++ src/com/xoste/leon/service/UserService.java
@@ -0,0 +1,15 @@
+package com.xoste.leon.service;
+
+import com.xoste.leon.pojo.User;
+
+/**
+ * @author Xoste
+ */
+public interface UserService {
+    /**
+     * 添加用户
+     * @param user User
+     * @return User
+     */
+    User addUser(User user);
+}
---
+++ src/com/xoste/leon/service/impl/UserServiceImpl.java
@@ -0,0 +1,14 @@
+package com.xoste.leon.service.impl;
+
+import com.xoste.leon.pojo.User;
+import com.xoste.leon.service.UserService;
+
+/**
+ * @author Xoste
+ */
+public class UserServiceImpl implements UserService {
+    @Override
+    public User addUser(User user) {
+        return null;
+    }
+}
